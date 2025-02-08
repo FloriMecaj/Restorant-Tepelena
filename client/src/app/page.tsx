90164572e8527f87ed73d3fa068227f94ff61266
FloriMecaj <florimecaj31@gmail.com>
2025-02-08 02:14:30 +0100

    metadata added

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,22 +1,25 @@
 import React from "react";
-import { HeroBanner } from "../components/HeroBanner/HeroBanner";
-import { AboutUs } from "../components/AboutUs/AboutUs";
+import { HeroBanner } from "@/components/HeroBanner/HeroBanner";
+import { AboutUs } from "@/components/AboutUs/AboutUs";
 import { MiniMenu } from "@/components/MiniMenu/MiniMenu";
 import { Maps } from "@/components/Maps/Maps";
 import { Footer } from "@/components/Footer/Footer";
 import { Speciality } from "@/components/Speciality/Speciality";
+import { metadata } from "@/components/Metadata/metadata"; // Import main metadata
 
-const Home: React.FC = () => {
+export { metadata };
+
+const Page = () => {
   return (
-    <>
+    <div className="w-full h-full">
       <HeroBanner />
       <AboutUs />
       <MiniMenu />
       <Speciality />
       <Maps />
       <Footer />
-    </>
+    </div>
   );
 };
 
-export default Home;
+export default Page;
