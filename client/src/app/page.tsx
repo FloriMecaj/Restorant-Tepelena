4cbd0bd507d66c7158c8afbf75a64d07125ad029
FloriMecaj <florimecaj31@gmail.com>
2025-02-08 02:02:37 +0100

    metadata removed

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -5,13 +5,6 @@ import { MiniMenu } from "@/components/MiniMenu/MiniMenu";
 import { Maps } from "@/components/Maps/Maps";
 import { Footer } from "@/components/Footer/Footer";
 import { Speciality } from "@/components/Speciality/Speciality";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Restorant Tepelena",
-  description:
-    "Taste the finest traditional Albanian dishes at Restaurant Tepelena in Tirana, operating since 1998.",
-};
 
 const Home: React.FC = () => {
   return (
