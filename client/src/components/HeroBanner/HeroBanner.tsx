f034a7ed7522982ef662c214cbfc23e19c985fdf
FloriMecaj <florimecaj31@gmail.com>
2025-02-11 14:39:23 +0100

    scroll glitch fix

--- client/src/components/HeroBanner/HeroBanner.tsx
+++ client/src/components/HeroBanner/HeroBanner.tsx
@@ -8,7 +8,7 @@ export const HeroBanner = () => {
   return (
     <div
       id="hero-banner"
-      className="w-full h-screen flex flex-col relative overflow-hidden"
+      className="w-full h-lvh flex flex-col relative overflow-hidden"
     >
       <Header />
       <div className="fixed left-0 top-0 right-0 bottom-0 -z-10 bg-gradient-to-b from-black/70 to-black/20" />
